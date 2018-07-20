f0b7436273a0bf2cb315734f8b50a482083e816f
jairorudas <jairorudasp@gmail.com>
2018-07-20 12:33:17 -0300

    changed port for application

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3005,
     db: process.env.MONGODB_URI || 'mongodb://localhost:27017/tvshows',
     SECRET_TOKEN: 'TQZqd4GBmA2283eg'
 }
