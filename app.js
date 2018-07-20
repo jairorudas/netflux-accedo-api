1e3613c4b6a8c8eda9020285ef5a8b7e76700f32
jairorudas <jairorudasp@gmail.com>
2018-07-20 16:42:08 -0300

    Enable cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 const api = require('./routes')
 
+app.use(cors)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
