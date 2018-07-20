8c245aa8bb43d9cbf54340ea0ee7fa1d478dbb52
jairorudas <jairorudasp@gmail.com>
2018-07-20 16:38:55 -0300

    sync with db disabled

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = require('./app')
 const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
-    if (err) {
-        return console.log(`Error connection database: ${err}`)
-    }
-    console.log('connection success db')
+    // if (err) {
+    //     return console.log(`Error connection database: ${err}`)
+    // }
+    // console.log('connection success db')
 
     app.listen(config.port, () => {
         console.log(`API REST online in http://localhost:${config.port}`)
